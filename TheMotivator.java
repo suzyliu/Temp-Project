2bab1e004c065031a7b8ec0e1a777b992f504862
SzuYu Liu <szuyu96@gmail.com>
2016-04-09 16:46:37 -0700

    Add multiple events

--- TheMotivator.java
+++ TheMotivator.java
@@ -1,4 +1,5 @@
 public class TheMotivator {
+	private ArrayList<String> events = new ArrayList<String>();
 	public void thoughtForTheDay (int score) {
 		System.out.println("Some new thoughts!");
 		if (score == 100)
@@ -13,9 +14,12 @@ public class TheMotivator {
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
 		tm.upcomingEvents();
+		tm.thoughtForTheDay(100);
+		tm.createEvents();
 	}
 	public void upcomingEvents(){
 		System.out.println("Kind of header for the the motivator");
+		for (String event:events)
 		 System.out.println("Party blabla");
 	}
 }
